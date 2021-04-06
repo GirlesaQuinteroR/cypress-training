25bfa3dd247c1df2b80b5d1d3ed5424dddc775fb
GirlesaQR <girle0708@gmail.com>
2021-04-06 18:59:23 -0500

    changes to index.js file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,16 @@
 /// <reference types="cypress" />
-
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
 const wp = require('@cypress/webpack-preprocessor')
-
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -9,6 +18,5 @@ module.exports = (on, config) => {
   const options = {
     webpackOptions: require('../../webpack.config'),
   }
-
   on('file:preprocessor', wp(options))
 }
