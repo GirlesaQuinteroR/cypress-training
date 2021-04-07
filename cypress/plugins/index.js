e2373dbbbb78c6b4415ddc4c3266f81ddf7b3222
GirlesaQR <girle0708@gmail.com>
2021-04-06 19:43:57 -0500

    index.js changes

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,23 +8,9 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
-
-/// <reference types="cypress" />
-
 const wp = require('@cypress/webpack-preprocessor')
-
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -32,6 +18,5 @@ module.exports = (on, config) => {
   const options = {
     webpackOptions: require('../../webpack.config'),
   }
-
   on('file:preprocessor', wp(options))
 }
